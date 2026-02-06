808a2b0a7eb853bb312b354c7e5852d9ceecf373
hellonitin12345 <hellonitin12345@gmail.com>
2026-02-06 18:59:05 +0530

    update

--- app/layout.js
+++ app/layout.js
@@ -28,7 +28,7 @@ export default function RootLayout({ children }) {
         
         <Navbar/>
         {children}
-        {/* <Footer/>  */}
+        <Footer/>  
         
       </body>
     </html>
--- components/common/Footer.jsx
+++ components/common/Footer.jsx
@@ -2,19 +2,19 @@ import { Instagram, Linkedin, X } from "lucide-react";
 
 const Footer = () => {
   return (
-    <section className="w-full bg-white pt-15">
+    <section className="w-full bg-white pt-15 ">
       {/* MAIN BLUE AREA */}
-      <div className="h-full md:mx-1 lg:mx-2 rounded-b-2xl bg-linear-to-r from-[#A1DAFF] to-[#B9E2FC]  overflow-hidden">
+      <div className=" relative h-100 md:full md:mx-1 lg:mx-2 rounded-b-2xl bg-linear-to-r from-[#A1DAFF] to-[#B9E2FC]  overflow-hidden">
 
         {/* TEXT */}
          <h1 className="text-center text-white font-clauson font-medium lg:text-9xl
-         sm:text-4xl md:text-5xl ">
+         sm:text-4xl md:text-5xl lg:-mt-18 lg:mb-55 lg:leading-33 lg:tracking-[-5%]">
           No paper work<br /> 
           no waiting rooms.<br />
           Just quick scan,
          </h1>
 
-         <img className="absolute bottom-0  lg:left-115 w-115" 
+         <img className="absolute bottom-0  w-50  lg:left-115 lg:w-115" 
          src="/document.png" alt="" />
       </div>
 
